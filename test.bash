7500b9cbffb725fd0ea6628dd1ed75f13e0701ab
Ryoya Sato <satoryoya1012711@gmail.com>
2026-03-25 23:08:35 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,6 +1,21 @@
 #!/bin/bash -xv
+# SPDX-FileCopyrightText: 2026 Ryoya Sato
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+###I/O test###
 
 out=$(seq 5 | ./plus)
 
-[ "${out}" = 15 ]
+[ "${out}" = 15 ] || ng $LINENO
+
+[ "${res}" = 0 ] && echo OK
+
+exit $res
 
